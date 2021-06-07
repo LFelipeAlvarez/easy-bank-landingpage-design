9334e745f8ac0484032009d22ebcc5007035fc05
LFelipeAlvarez <lufealsu@hotmail.com>
2021-06-07 09:04:50 -0500

    design again

--- dist/src.49b85e08.js
+++ dist/src.49b85e08.js
@@ -131,4 +131,4 @@ $burgerButton.addEventListener('click', function () {
   $burgerButton.classList.toggle('header__burger-button--active');
 });
 },{"./styles/app.scss":"OXm4"}]},{},["Focm"], null)
-//# sourceMappingURL=/src.378604e3.js.map
+//# sourceMappingURL=/src.49b85e08.js.map
